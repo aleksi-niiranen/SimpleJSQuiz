758943294ee6baab0c9747ef38b3cd0f510f883b
Aleksi Niiranen <aleksi.niiranen@gmail.com>
2012-02-24 18:30:40 +0200

    changed site structure, bug fixes, uses jQuery now for some things

--- quiz.js
+++ quiz.js
@@ -5,86 +5,23 @@ var xmlDoc;
 var questionDocumentElement;
 
 function addScore(arg) {
-	score += arg;
+	// arg is passed as String from XML document
+	// parse to int
+	score += parseInt(arg.data.param, 10);
 	question++;
-	nextQuestion();
+	renderContent();
 }
 
 function loadQuiz(url) {
-	document.getElementById("startService").style.display = "none";
-	document.getElementById("startProduct").style.display = "none";
-	document.getElementById("images").style.display = "inline";
 	var xmlhttp;
 	if (window.XMLHttpRequest) {
 		xmlhttp = new XMLHttpRequest();
-		xmlhttp.onreadystatechange = function() {
-			if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-				xmlDoc = xmlhttp.responseXML;
-				questionDocumentElement = xmlDoc.documentElement.getElementsByTagName("QUESTION");
-				questionsCount = questionDocumentElement.length;
-				// load first question
-				renderContent();
-			}
-		}
-		xmlhttp.open("GET", url, true);
+		xmlhttp.open("GET", url, false);
 		xmlhttp.send();
+		xmlDoc = xmlhttp.responseXML;
+		questionDocumentElement = xmlDoc.documentElement.getElementsByTagName("question");
+		questionsCount = questionDocumentElement.length;
 	} else {
 		// IE6 not supported
 	}
-}
-
-function nextQuestion() {
-	if (question < questionsCount) {
-		renderContent();
-	} else {
-		document.getElementById("question").innerHTML = "Your score was " + score + "!";
-		document.getElementById("images").style.display = "none";
-	}
-}
-
-function renderContent() {
-	var xx = questionDocumentElement[question].getElementsByTagName("TITLE");
-	try {
-		document.getElementById("question").innerHTML = xx[0].firstChild.nodeValue;
-	} catch (err) { }
-
-	xx = questionDocumentElement[question].getElementsByTagName("OPTION_ONE");
-	try {
-		if (xx[0].hasChildNodes()) {
-			document.images["option1"].style.display = "inline";
-			document.images["option1"].src = xx[0].firstChild.nodeValue;
-		} else {
-			document.images["option1"].style.display = "none";
-		}
-	} catch (err) { }
-
-	xx = questionDocumentElement[question].getElementsByTagName("OPTION_TWO");
-	try {
-		if (xx[0].hasChildNodes()) {
-			document.images["option2"].style.display = "inline";
-			document.images["option2"].src = xx[0].firstChild.nodeValue;
-		} else {
-			document.images["option2"].style.display = "none";
-		}
-	} catch (err) { }
-
-	xx = questionDocumentElement[question].getElementsByTagName("OPTION_THREE");
-	try {
-		if (xx[0].hasChildNodes()) {
-			document.images["option3"].style.display = "inline";
-			document.images["option3"].src = xx[0].firstChild.nodeValue;
-		} else {
-			document.images["option3"].style.display = "none";
-		}
-	} catch (err) { }
-
-	xx = questionDocumentElement[question].getElementsByTagName("OPTION_FOUR");
-	try {
-		if (xx[0].hasChildNodes()) {
-			document.images["option4"].style.display = "inline";
-			document.images["option4"].src = xx[0].firstChild.nodeValue;
-		} else {
-			document.images["option4"].style.display = "none";
-		}
-	} catch (err) { }
 }
